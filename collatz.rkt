0fd5a88be212828c139f9fe80ecf1a6e833ed64a
Daynel-Kem <50426423+Daynel-Kem@users.noreply.github.com>
2025-02-19 20:21:20 -0500

    Update collatz.rkt

--- collatz.rkt
+++ collatz.rkt
@@ -1,4 +1,4 @@
-;; (collatz n) based on Collatz' Conjecture, will return a list of the numbers until it reaches the 4 2 1 loop
+;; (collatz n) based on Collatz' Conjecture, will return a list of numbers produced by the procedure until it reaches the 4-2-1 loop
 ;; Examples:
 (check-expect (collatz 4) (list 4 2 1))
 (check-expect (collatz 13) (list 13 40 20 10 5 16 8 4 2 1))
